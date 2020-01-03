467972f3740570731bbaff141f77bd58cfdf4560
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-01-03 23:24:41 +0000

    Tests and fixes for empty() constructor

--- reactive/constructors.py
+++ reactive/constructors.py
@@ -1,4 +1,4 @@
-from typing import AsyncIterable, Iterable, TypeVar
+from typing import AsyncIterable, AsyncIterator, Iterable, NoReturn, TypeVar
 
 T = TypeVar("T")
 
@@ -7,8 +7,17 @@ async def from_iterable(it: Iterable[T]) -> AsyncIterable[T]:
     for x in it:
         yield x
 
-async def empty() -> AsyncIterable[None]:
-    yield
+
+def empty() -> AsyncIterable[None]:
+    class Empty(AsyncIterator[None]):
+        def __aiter__(self) -> AsyncIterator[None]:
+            return self
+
+        async def __anext__(self) -> NoReturn:
+            raise StopAsyncIteration
+
+    return Empty()
+
 
 def just(*args: T) -> AsyncIterable[T]:
     return from_iterable(args)
--- reactive_tests/test_constructors.py
+++ reactive_tests/test_constructors.py
@@ -1,8 +1,28 @@
+import asyncio
 import unittest
 
+import aiounittest
 
-class TestConstructors(unittest.TestCase):
-    pass
+import reactive
+from typing import List
+
+
+class TestConstructors(aiounittest.AsyncTestCase):
+    async def test_empty(self) -> None:
+        empty = reactive.empty()
+
+        values: List[None] = []
+        async for x in empty:
+            values.append(x)
+
+        self.assertEqual(values, [])
+
+    async def test_empty_manual_iteration(self) -> None:
+        empty = reactive.empty()
+
+        it = empty.__aiter__()
+        with self.assertRaises(StopAsyncIteration):
+            await it.__anext__()
 
 
 if __name__ == "__main__":
