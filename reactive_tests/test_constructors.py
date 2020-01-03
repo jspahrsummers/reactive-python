555ea0970a5c78bb06f069d586ed04da8ad0623d
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-01-03 23:12:12 +0000

    Stub tests

---
+++ reactive_tests/test_constructors.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestConstructors(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ reactive_tests/test_operators.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestOperators(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
