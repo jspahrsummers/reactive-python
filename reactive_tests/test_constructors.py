a94d9e24effdecfc3cb5448913f976bb0a528789
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-01-04 00:04:18 +0000

    Test from_iterable

--- reactive_tests/test_constructors.py
+++ reactive_tests/test_constructors.py
@@ -1,10 +1,12 @@
 import asyncio
 import unittest
+from typing import Any, Callable, Iterable, List, no_type_check_decorator
 
 import aiounittest
+from hypothesis import given
+from hypothesis.strategies import from_type, iterables
 
 import reactive
-from typing import List
 
 
 class TestConstructors(aiounittest.AsyncTestCase):
@@ -20,9 +22,32 @@ class TestConstructors(aiounittest.AsyncTestCase):
     async def test_empty_manual_iteration(self) -> None:
         empty = reactive.empty()
 
-        it = empty.__aiter__()
+        ait = empty.__aiter__()
         with self.assertRaises(StopAsyncIteration):
-            await it.__anext__()
+            await ait.__anext__()
+
+    @given(iterables(from_type(type)))  # type: ignore
+    async def test_from_iterable(self, it: Iterable[Any]) -> None:
+        # Save aside values as we iterate, so we can test that from_iterable() works with any type of iterable while also validating the output.
+        values: List[Any] = []
+
+        def _save(x: Any) -> Any:
+            nonlocal values
+            values.append(x)
+            return x
+
+        ait = reactive.from_iterable((_save(x) for x in it))
+        async for x in ait:
+            self.assertEqual(x, values.pop(0))
+
+    # Hypothesis hook
+    def execute_example(self, fn: Callable[[], Any]) -> Any:
+        result = fn()
+
+        if asyncio.iscoroutine(result):
+            result = asyncio.run(result)
+
+        return result
 
 
 if __name__ == "__main__":
