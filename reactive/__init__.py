6e2ea24d97285e0f4dfa9df17c906cb34162a505
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-01-04 12:52:37 +0000

    Move stream primitives into their own module

--- reactive/__init__.py
+++ reactive/__init__.py
@@ -1,4 +1,14 @@
 from . import operators as op
 from .constructors import empty, from_iterable, just
+from .stream import StreamGenerator, chain, connect
 
-__all__ = ["empty", "from_iterable", "just", "op"]
+__all__ = [
+    "AwaitableIterable",
+    "chain",
+    "connect",
+    "empty",
+    "from_iterable",
+    "just",
+    "op",
+    "StreamGenerator",
+]
