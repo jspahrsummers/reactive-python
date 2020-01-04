dff7b13922ad95b30568521bf0d9edce40baf69a
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-01-04 00:22:33 +0000

    Test map()

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,8 +1,25 @@
 import unittest
+from typing import Callable, List
+
+from hypothesis import given
+from hypothesis.strategies import integers, lists
+
+import reactive
+import tests.helpers
+from reactive import op
 
 
-class TestOperators(unittest.TestCase):
-    pass
+class TestOperators(tests.helpers.AsyncTestCase):
+    @given(lists(integers()))  # type: ignore
+    async def test_map(self, items: List[int]) -> None:
+        # mypy needs help to infer map() here :(
+        fn: Callable[[int], int] = lambda x: x * 2
+        ait = op.map(fn)(reactive.from_iterable(items))
+
+        i = 0
+        async for x in ait:
+            self.assertEqual(x, items[i] * 2)
+            i += 1
 
 
 if __name__ == "__main__":
