1fb96eefbb1a1f8ad404e5891082144d07e6f09a
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-01-04 13:17:54 +0000

    Code documentation

--- reactive/aitertools.py
+++ reactive/aitertools.py
@@ -4,11 +4,19 @@ T = TypeVar("T")
 
 
 async def from_iterable(it: Iterable[T]) -> AsyncIterable[T]:
+    """
+    Lifts a synchronous iterable into an asynchronous iterable, without otherwise changing its behavior. This is useful for composing with async-only functionality.
+    """
+
     for x in it:
         yield x
 
 
 def empty() -> AsyncIterable[None]:
+    """
+    Returns an asynchronous iterable that yields zero values.
+    """
+
     class Empty(AsyncIterator[None]):
         def __aiter__(self) -> AsyncIterator[None]:
             return self
@@ -20,4 +28,8 @@ def empty() -> AsyncIterable[None]:
 
 
 def just(*args: T) -> AsyncIterable[T]:
+    """
+    Returns an asynchronous iterable which yields the given values, in order.
+    """
+
     return from_iterable(args)
--- reactive/operators.py
+++ reactive/operators.py
@@ -8,6 +8,12 @@ TOut = TypeVar("TOut", covariant=True)
 
 
 def map(fn: Callable[[TIn], TOut]) -> StreamGenerator[TOut, TIn]:
+    """
+    Creates a stream generator which maps over its inputs using the given function.
+    
+    Each stream yielded will have exactly one item: the mapped value.
+    """
+
     async def _map(fn: Callable[[TIn], TOut]) -> AsyncGenerator[Iterable[TOut], TIn]:
         out_value: Iterable[TOut] = []
 
@@ -19,6 +25,12 @@ def map(fn: Callable[[TIn], TOut]) -> StreamGenerator[TOut, TIn]:
 
 
 def filter(fn: Callable[[T], bool]) -> StreamGenerator[T, T]:
+    """
+    Creates a stream generator which filters its inputs using the given predicate.
+
+    Only inputs matching the predicate will be included in the output. Each stream yielded may have zero or one value, depending on whether the predicate passed on the input.
+    """
+
     async def _filter(fn: Callable[[T], bool]) -> AsyncGenerator[Iterable[T], T]:
         out_value: Iterable[T] = []
 
@@ -30,6 +42,12 @@ def filter(fn: Callable[[T], bool]) -> StreamGenerator[T, T]:
 
 
 def collect() -> StreamGenerator[List[T], T]:
+    """
+    Creates a stream generator which accumulates all of its inputs into a single list, which is yielded only after the full input is consumed.
+
+    While the input is still being consumed, each stream yielded will have zero values. Once the input is exhausted, one stream will be yielded containing one list of all the input values received.
+    """
+
     async def _collect() -> AsyncGenerator[Iterable[List[T]], T]:
         values: List[T] = []
 
